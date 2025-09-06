b2d701cb51dfd866de91264d5ec5bf116bc9ba24
Akshar2007 <88934310+Akshar2007@users.noreply.github.com>
2025-09-06 18:12:36 +0530

    Create small&large for three values.c

---
+++ small&large for three values.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter three numbers:");
+scanf("%d%d%d",&a,&b,&c);
+if (a>b && a>c)
+{
+printf("%d is large",a);
+}
+else if(b>a && b>c)
+{
+printf("%d is large",b);
+}
+else if(c>a && c>b)
+{
+printf("%d is large",c);
+}
+if (a<b && a<c)
+{
+    printf ("%d is small",a);
+}
+else if (b<a && b<c)   
+{
+    printf ("%d is small",b);
+}
+else if (c<a && c<b)
+{
+    printf ("%d is small",c);
+}
+
+return 0;
+
+}
