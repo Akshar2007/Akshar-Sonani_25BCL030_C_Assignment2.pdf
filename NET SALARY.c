84239191ae3ce4b744a96da3d25f58ab6e008aca
Akshar2007 <88934310+Akshar2007@users.noreply.github.com>
2025-09-07 10:16:21 +0530

    Create NET SALARY.c

---
+++ NET SALARY.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter GROSS SALARY  ALLOWANCES   DEDUCTIONS:");
+scanf("%d%d%d",&a,&b,&c);
+printf("NET SALARY IS %d",a+b-c);
+
+return 0;
+
+}
